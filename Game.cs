78706e72320d131f168180994521a1497b80258c
Gus <gusgit@fastmail.org>
2023-11-25 13:59:28 +0100

    Add collision check

--- Game.cs
+++ Game.cs
@@ -37,16 +37,16 @@ class Game
         switch (key)
         {
             case ConsoleKey.UpArrow:
-                player.Move(0, -1);
+                MovePlayer(0, -1);
                 break;
             case ConsoleKey.DownArrow:
-                player.Move(0, 1);
+                MovePlayer(0, 1);
                 break;
             case ConsoleKey.LeftArrow:
-                player.Move(-1, 0);
+                MovePlayer(-1, 0);
                 break;
             case ConsoleKey.RightArrow:
-                player.Move(1, 0);
+                MovePlayer(1, 0);
                 break;
             default:
                 break;
@@ -57,84 +57,11 @@ class Game
     private void MovePlayer(int x, int y)
     {
         //Check if the player can move to the new position
-        if (worldMap.GetSymbolAt((int)player.Position.X + x, (int)player.Position.Y + y) == '.')
+        if (worldMap.CanMoveTo(player.Position + new Vector2(x, y)))
         {
-            //Move the player
-            player.Position += new Vector2(x, y);
+            player.Move(x, y);
         }
     }
-
-    // public void Draw()
-    // {
-    //     //Set cursors to top left
-    //     Console.SetCursorPosition(0, 0);
-    //     int halfViewWidth = viewWidth / 2;
-    //     int halfViewHeight = viewHeight / 2;
-
-    //     // Calculate the top-left position of the visible area
-    //     int startX = (int)Math.Max(0, player.Position.X - halfViewWidth);
-    //     int startY = (int)Math.Max(0, player.Position.Y - halfViewHeight);
-
-    //     // Adjust start positions to avoid going beyond map boundaries
-    //     startX = Math.Min(startX, worldMap.MapWidth - viewWidth);
-    //     startY = Math.Min(startY, worldMap.MapWidth - viewHeight);
-
-    //     char symbol;
-    //     for (int y = startY; y < startY + viewHeight; y++)
-    //     {
-    //         for (int x = startX; x < startX + viewWidth; x++)
-    //         {
-    //             //Draw the player symbol at the player position
-    //             if (player.Position.X == x && player.Position.Y == y)
-    //             {
-    //                 symbol = player.Symbol;
-    //             }
-    //             else
-    //             {
-    //                 //Otherwise draw the map tile
-    //                 symbol = worldMap.GetSymbolAt(x, y);
-    //             }
-
-    //             Console.Write(symbol);
-    //         }
-    //         Console.WriteLine();
-    //     }
-    // }
-
-    // public void Draw()
-    // {
-    //     StringBuilder stringBuilder = new StringBuilder();
-
-    //     int halfViewWidth = viewWidth / 2;
-    //     int halfViewHeight = viewHeight / 2;
-
-    //     // Calculate the top-left position of the visible area
-    //     int startX = (int)Math.Max(0, player.Position.X - halfViewWidth);
-    //     int startY = (int)Math.Max(0, player.Position.Y - halfViewHeight);
-
-    //     // Adjust start positions to avoid going beyond map boundaries
-    //     startX = Math.Min(startX, worldMap.MapWidth - viewWidth);
-    //     startY = Math.Min(startY, worldMap.MapWidth - viewHeight);
-
-    //     for (int y = startY; y < startY + viewHeight; y++)
-    //     {
-    //         for (int x = startX; x < startX + viewWidth; x++)
-    //         {
-    //             char symbol = worldMap.GetSymbolAt(x, y);
-
-    //             if (player.Position.X == x && player.Position.Y == y)
-    //             {
-    //                 symbol = player.Symbol;
-    //             }
-
-    //             stringBuilder.Append(symbol);
-    //         }
-    //         stringBuilder.AppendLine();
-    //     }
-
-    //     Console.SetCursorPosition(0, 0);
-    //     Console.Write(stringBuilder.ToString());
-    // }
 
     public void Draw()
     {
--- WorldMap.cs
+++ WorldMap.cs
@@ -1,11 +1,15 @@
+using System.Numerics;
+
 class WorldMap
 {
     public char[,] mapTiles;
     public int MapWidth => mapTiles.GetLength(0);
     private List<GameEntity> entities = new List<GameEntity>();
+    private Dictionary<Vector2, GameEntity> entityPositionMap;
 
     public WorldMap(int mapWidth)
     {
+        entityPositionMap = new Dictionary<Vector2, GameEntity>();
         entities = new List<GameEntity>();
         mapTiles = new char[mapWidth, mapWidth];
         GenerateMap();
@@ -19,51 +23,72 @@ class WorldMap
         {
             for (int y = 0; y < MapWidth; y++)
             {
-                if (Random.Next(0, 10) == 0)
-                {
-                    mapTiles[x, y] = ',';
-                }
-                else
-                {
-                    mapTiles[x, y] = '.';
-                }
+                // if (Random.Next(0, 10) == 0)
+                // {
+                //     mapTiles[x, y] = ',';
+                // }
+                // else
+                // {
+                mapTiles[x, y] = '.';
+                // }
 
-                if (Random.Next(0, 100) == 0)
+                if (Random.Next(0, 50) == 0)
                 {
-                    entities.Add(new Tree(x, y));
+                    AddEntity(new Tree(x, y));
                 }
                 else if (Random.Next(0, 100) == 0)
                 {
-                    entities.Add(new Rock(x, y));
+                    AddEntity(new Rock(x, y));
                 }
-                else if (Random.Next(0, 100) == 0)
+                else if (Random.Next(0, 200) == 0)
                 {
-                    entities.Add(new Enemy(x, y));
+                    AddEntity(new Enemy(x, y));
                 }
             }
         }
+    }
 
-        // Update: entities.Add(new Tree(x, y)); or entities.Add(new Enemy(x, y));
+    public void AddEntity(GameEntity entity)
+    {
+        entityPositionMap[entity.Position] = entity;
+        entities.Add(entity);
+    }
+
+    public void UpdateEntityPosition(GameEntity entity, Vector2 newPosition)
+    {
+        entityPositionMap.Remove(entity.Position);
+        entity.Position = newPosition;
+        entityPositionMap[entity.Position] = entity;
     }
 
     internal char GetSymbolAt(int x, int y)
     {
-        char symbol = mapTiles[x, y];
-        GameEntity? entity = entities.FirstOrDefault(e => e.Position.X == x && e.Position.Y == y);
-        if (entity != null)
+        char symbol;
+        Vector2 position = new Vector2(x, y);
+
+        if (entityPositionMap.TryGetValue(position, out GameEntity? entity))
         {
-            symbol = entity.Symbol; // Override symbol if an entity is present
+            symbol = entity.Symbol;
+        }
+        else
+        {
+            symbol = mapTiles[x, y];
         }
         return symbol;
     }
 
-    // public void Update()
-    // {
-    // foreach (var entity in entities)
-    // {
-    //     entity.Update();
-    // }
-    // }
+    internal bool CanMoveTo(Vector2 vector2)
+    {
+        if (vector2.X < 0 || vector2.X >= MapWidth || vector2.Y < 0 || vector2.Y >= MapWidth)
+        {
+            return false;
+        }
 
-    // Additional methods like collision detection, enemy movement, etc.
+        if (entityPositionMap.TryGetValue(vector2, out GameEntity? entity))
+        {
+            return !entity.IsBlocking;
+        }
+
+        return true;
+    }
 }
